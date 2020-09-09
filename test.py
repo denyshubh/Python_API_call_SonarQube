d2754bbbb4a5829cdfdbaf0768bade1cae74a6ed
Shubham Kumar Singh <37653026+denyshubh@users.noreply.github.com>
2020-09-09 15:58:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,16 +6,28 @@ It needs admin user privilages
 You can either use Token or use your username and password
 To use Token add token in the user prompt and leave password field empty
 '''
-import sys
+
+import os
 import requests
 from requests.exceptions import HTTPError
+from getpass import getpass
 
-
-# TOKEN = "d736a9c5368d4d3906505655191a403cb08bbee2"
-SONARQUBE_QUALITYGATE_API = sys.argv[0] + "/api/qualitygates"
-USER = sys.argv[1]
-PASS = sys.argv[2]
-QUALITY_GATE_NAME = sys.argv[3]
+TOKEN = os.environ['SONAR_AUTH_TOKEN']
+# should be replaced by sonarqube Jenking configuration's fields/variables
+SONARQUBE_URL = os.environ['SONAR_HOST_URL']
+#input("SonarQube URL like http://localhost:9000: ")
+SONARQUBE_QUALITYGATE_API = SONARQUBE_URL+"/api/qualitygates"
+SONARQUBE_QUALITYPROFILE_API = SONARQUBE_URL+"/api/qualityprofiles"
+# should be replaced by sonarqube Jenking configuration's fields/variables
+#USER = input('User: ')
+#PASS = getpass()
+# The new name of QG of on-boarding team. Should 'Lean SDLC_<TeamName>'
+QUALITY_GATE_NAME = "Lean SDLC_HITT_EXTENDED_New2"
+#input("Quality Gate Name: ")
+# Default Name of Sonar plus Mutation Quality Profile created by HITT as the DEFAULT
+QUALITY_PROFILE_NAME_FROM = "Sonar+Mutation" 
+# The new name of profile on-boarding team wants. Should HITT the DEFAULT + _<TeamName>
+QUALITY_PROFILE_NAME_TO = "Sonar+Mutation_HITT_EXTENDED_New2"
 
 CONDITIONS = {
     0: ["coverage", "LT", "100"],
@@ -34,11 +46,12 @@ def create_quality_gate(quality_gate_name):
     '''
     This function creates a Quality Gate
     '''
+    print('In Create')
     try:
         res = requests.post(
             '{}/create'.format(SONARQUBE_QUALITYGATE_API),
             params={'name':quality_gate_name},
-            auth=(USER, PASS)
+            auth=(TOKEN, '')
             )
         res.raise_for_status()
         print(
@@ -61,10 +74,11 @@ def add_conditions(gate_id, metric, operator, error):
     '''
     This function adds condition to our quality gate
     '''
+    print('In Add Condition')
     res = requests.post(
         '{}/create_condition'.format(SONARQUBE_QUALITYGATE_API),
         params=[('gateId', gate_id), ('metric', metric), ('op', operator), ('error', error)],
-        auth=(USER, PASS)
+        auth=(TOKEN, '')
         )
     print(res.text)
     return res.status_code
@@ -76,14 +90,76 @@ def set_qg_default(gate_id):
     res = requests.post(
         '{}/set_as_default'.format(SONARQUBE_QUALITYGATE_API),
         params={'id':gate_id},
-        auth=(USER, PASS)
+        auth=(TOKEN, '')
         )
     print(res.text)  
     return res.status_code
+    
+def search_qprofile(qp_name):
+    '''
+    This function searches for the ID of QP for a given QP Name
+    '''
+    try:
+        res = requests.get(
+            '{}/search'.format(SONARQUBE_QUALITYPROFILE_API),
+            params={'qualityProfile':qp_name},
+            auth=(TOKEN, '')
+            )
+        res.raise_for_status()
+        print(
+            '\nQuality Profile {} found\n status: {}\n response:{}'
+            .format(qp_name, res.status_code, res.text)
+            )
+        return res.json()['profiles'][0]['key']
+    except HTTPError as http_error:
+        print(
+            'Quality Profile not found : {}'
+            .format(http_error)
+            )
+    except Exception as err:
+        print("Error in the Code", err)
 
-if __name__ == "__main__":
+    return None
+    
+def copy_qprofile(qp_id, newProfileName):
+    '''
+    This function will copy the SonarAndMutation profile for an on boarding team into a provide QP name
+    '''
+    print(
+        '\nCopying {} to {}\n'
+        .format(qp_id, newProfileName)
+        )
+    res = requests.post(
+        '{}/copy'.format(SONARQUBE_QUALITYPROFILE_API),
+        params=[('fromKey', qp_id), ('toName', newProfileName)],
+        auth=(TOKEN, '')
+        )
+    print(res.text)  
+    return res.status_code
+    
+'''
+def delete_qprofile(profileName):
+    
+    print(
+        'Deleting {}\n'
+        .format(profileName)
+        )
+    res = requests.post(
+        '{}/delete'.format(SONARQUBE_QUALITYPROFILE_API),
+        params={'profileKey': profileName},
+        auth=(TOKEN, '')
+        )
+    print(res.text)  
+    return res.status_code
+'''
+def _main_() :
+    print(
+        'In Main\n{}\n{}'
+        .format(SONARQUBE_URL, TOKEN)
+         )
     try:
         gate_id = create_quality_gate(QUALITY_GATE_NAME)
+        
         if gate_id:
             for _, val in CONDITIONS.items():
                 result = add_conditions(gate_id, val[0], val[1], val[2])
@@ -92,12 +168,17 @@ if __name__ == "__main__":
             print(
                 'Conditions added successfully to Quality Gate'
                 )
-            result = set_qg_default(gate_id)
+            '''result = set_qg_default(gate_id)'''
+            qp_id = search_qprofile(QUALITY_PROFILE_NAME_FROM)
+            result = copy_qprofile(qp_id, QUALITY_PROFILE_NAME_TO)
             if result & 200 != 200:
                 raise ValueError('Error while making the quality gate as default')
             print(
-                'Quality Gate with ID {} is set as default'
-                .format(gate_id)
+                'Quality Profile with Name {} is copied from default one - {}'
+                .format(QUALITY_PROFILE_NAME_TO, qp_id)
                 )
+            #delResult = delete_qprofile('AXRx4ln2plT98XS9cSoM')
     except ValueError as error:
         print(error)
+        
+_main_()
